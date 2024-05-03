a55808c8e3c1cc08cc51e58d6edc81c29b21693b
Mads Klinkby <m@kli.dk>
2024-05-03 15:51:05 +0200

    add spam prevention script

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+﻿(function() {
+    document.querySelectorAll('a[href^="mailto:"]').forEach(el => {
+        el.setAttribute('href', el.getAttribute('href').replace('*', '@'));
+        el.innerHTML = el.innerHTML.replace('*', '@');
+    });
+})();
